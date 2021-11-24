b37e6d3434ba1787e030789012eaa17f1f594711
David Stiven <56278983+David-sqrtpi@users.noreply.github.com>
2021-11-24 14:49:37 -0500

    as

--- js/reserva.js
+++ js/reserva.js
@@ -1,6 +1,15 @@
 const formButton = document.querySelector("#form-submit");
 const form = document.querySelector("#formulario");
-const formTest = document.forms["formulario"];
+const f = document.forms["formulario"];
+const info = "f.elements: " + f.elements + "\n"
+         + "f.length: " + f.length + "\n"
+         + "f.name: " + f.elements + "\n"
+         + "f.acceptCharset: " + f.acceptCharset + "\n"
+         + "f.action: " + f.action + "\n"
+         + "f.enctype: " + f.enctype + "\n"
+         + "f.encoding: " + f.encoding + "\n"
+         + "f.method: " + f.method + "\n"
+         + "f.target: " + f.target;
 formButton.addEventListener("click", onSubmit);
 // destino-select
 // llegada-select
@@ -12,5 +21,5 @@ formButton.addEventListener("click", onSubmit);
 function onSubmit(event) {
     event.preventDefault();
     console.log("funcionaaaA");
-    console.log("Form", formTest);
+    console.log("Form", f);
 }
