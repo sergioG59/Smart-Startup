552a514001496faeab4d3ea63edd73b8c1d42277
David Stiven <56278983+David-sqrtpi@users.noreply.github.com>
2021-11-24 14:58:37 -0500

    value

--- js/reserva.js
+++ js/reserva.js
@@ -18,5 +18,5 @@ function onSubmit(event) {
     console.log("departure", departure);
     console.log("return_", return_);
     console.log("round", round);
-    console.log("oneway", oneway.target);
+    console.log("oneway", oneway.value);
 }
