8514a98803d0bc6517c2cc3c67c637a81509dba0
David Stiven <56278983+David-sqrtpi@users.noreply.github.com>
2021-11-24 14:37:02 -0500

    test

--- js/reserva.js
+++ js/reserva.js
@@ -1,7 +1,9 @@
 const formButton = document.querySelector("#form-submit");
+const form = document.querySelector("#formulario");
 formButton.addEventListener("click", form);
 
 function form(event) {
     event.preventDefault();
     console.log("funcionaaaA");
+    console.log("Form", form);
 }
